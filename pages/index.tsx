97f8a020871e02cb7af07c6b4c28989e2f620c1e
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-18 23:37:51 -0500

    centered app

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,15 @@ import App from "../components/app";
 
 const Desktop1: NextPage = () => {
   return (
-    <div className="relative rounded-[18px] [background:linear-gradient(180deg,_#2b303b,_rgba(43,_48,_59,_0))] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full h-[700px] overflow-hidden">
-      <Logo />
-      <Auth />
-      <DarkModeButton />
-      <App />
+    <div className="relative [background:linear-gradient(180deg,_#2b303b,_rgba(43,_48,_59,_0))] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full h-[820px] overflow-hidden">
+      <div>
+        <Logo />
+        <Auth />
+        <DarkModeButton />
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <App />
+      </div>
     </div>
   );
 };
