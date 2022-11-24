cd47b15d1a0ad4549c3d8b6d076a960d71c43e36
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-24 14:21:42 -0500

    updated font

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,14 +3,11 @@ import type { NextPage } from "next";
 const Logo: NextPage = () => {
   return (
     <a
-      className="[text-decoration:none] absolute top-[39px] left-[40px] w-[227px] h-[48px] text-left text-lg text-gray-100 font-inter"
+      className="[text-decoration:none] absolute top-[39px] left-[40px] w-[227px] h-[48px] text-left text-lg text-gray-100 font-mono"
       href="https://paperhands.vercel.app/"
     >
-      <div className="absolute top-[62.5%] left-[0%] inline-block">
-        Powered by 0x
-      </div>
       <div className="absolute top-[0%] left-[0%] text-2xl inline-block">
-        PaperHands 🤲📃🤲
+        elderay
       </div>
     </a>
   );
