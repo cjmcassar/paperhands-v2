4efcf380f041f5f78870f50bb07c5e1331e86402
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-24 14:22:07 -0500

    added chartjs and react-chartjs

--- components/line-chart.tsx
+++ components/line-chart.tsx
@@ -1,410 +1,67 @@
 import type { NextPage } from "next";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Your Assets",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 100 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 100 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
 const LineChart: NextPage = () => {
   return (
     <div className="absolute h-[24.22%] w-[83.31%] top-[7.81%] right-[6.93%] bottom-[67.97%] left-[9.76%] text-center text-xs text-gray-200 font-roboto">
-      <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-[12px] bg-gray-300 shadow-[2px_2px_2px_rgba(0,_0,_0,_0.25)]" />
-      <img
-        className="absolute top-[11px] left-[33.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[73.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[112.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[152.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[191.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[229.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[269.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[308.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[348.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[387.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[426.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[466.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[505.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[100.5px] left-[35px] w-[481px] h-[1px]"
-        alt=""
-        src="../yaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[70.5px] left-[35px] w-[481px] h-[1px]"
-        alt=""
-        src="../yaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[41.5px] left-[35px] w-[481px] h-[1px]"
-        alt=""
-        src="../yaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[10.5px] left-[35px] w-[481px] h-[1px]"
-        alt=""
-        src="../yaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[100.5px] left-[35px] w-[481px] h-[1px]"
-        alt=""
-        src="../yaxis-grid.svg"
-      />
-      <img
-        className="absolute top-[11px] left-[33.5px] w-[1px] h-[90px]"
-        alt=""
-        src="../xaxis-grid.svg"
-      />
-      <div className="absolute top-[108px] left-[25px] inline-block w-[20px] h-[10px]">
-        Text
+      <div className="absolute h-full w-full rounded-[12px] bg-gray-300 shadow-[2px_2px_2px_rgba(0,_0,_0,_0.25)]">
+        <Line options={options} data={data} />;
       </div>
-      <div className="absolute top-[108px] left-[65px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[104px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[143px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[182px] inline-block w-[20px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[222px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[261px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[300px] inline-block w-[20px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[340px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[379px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[418px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[457px] inline-block w-[20px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[108px] left-[497px] inline-block w-[19px] h-[10px]">
-        Text
-      </div>
-      <div className="absolute top-[96px] left-[20px] text-sm text-right inline-block w-[5px] h-[10px]">
-        0
-      </div>
-      <div className="absolute top-[66px] left-[14px] text-sm text-right inline-block w-[11px] h-[11px]">
-        50
-      </div>
-      <div className="absolute top-[36px] left-[9px] text-sm text-right inline-block w-[16px] h-[10px]">
-        100
-      </div>
-      <div className="absolute top-[6px] left-[9px] text-sm text-right inline-block w-[16px] h-[10px]">
-        150
-      </div>
-      <img
-        className="absolute top-[41px] left-[34.52px] w-[481.48px] h-[60px]"
-        alt=""
-        src="../line.svg"
-      />
-      <img
-        className="absolute top-[92px] left-[31px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker.svg"
-      />
-      <img
-        className="absolute top-[84px] left-[41px] w-[7px] h-[6px]"
-        alt=""
-        src="../marker1.svg"
-      />
-      <img
-        className="absolute top-[64px] left-[50px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker2.svg"
-      />
-      <img
-        className="absolute top-[57px] left-[60px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker3.svg"
-      />
-      <img
-        className="absolute top-[73px] left-[70px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker4.svg"
-      />
-      <img
-        className="absolute top-[50px] left-[80px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker5.svg"
-      />
-      <img
-        className="absolute top-[43px] left-[90px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker6.svg"
-      />
-      <img
-        className="absolute top-[85px] left-[99px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker7.svg"
-      />
-      <img
-        className="absolute top-[94px] left-[109px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker7.svg"
-      />
-      <img
-        className="absolute top-[89px] left-[119px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker7.svg"
-      />
-      <img
-        className="absolute top-[40px] left-[129px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker10.svg"
-      />
-      <img
-        className="absolute top-[93px] left-[139px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker11.svg"
-      />
-      <img
-        className="absolute top-[41px] left-[148px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker10.svg"
-      />
-      <img
-        className="absolute top-[40px] left-[158px] w-[8px] h-[6px]"
-        alt=""
-        src="../marker13.svg"
-      />
-      <img
-        className="absolute top-[68px] left-[168px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[50px] left-[178px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker15.svg"
-      />
-      <img
-        className="absolute top-[93px] left-[188px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker16.svg"
-      />
-      <img
-        className="absolute top-[88px] left-[197px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[96px] left-[208px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[72px] left-[217px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[40px] left-[227px] w-[8px] h-[6px]"
-        alt=""
-        src="../marker13.svg"
-      />
-      <img
-        className="absolute top-[75px] left-[237px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker16.svg"
-      />
-      <img
-        className="absolute top-[52px] left-[246px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker22.svg"
-      />
-      <img
-        className="absolute top-[67px] left-[257px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[75px] left-[266px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[71px] left-[276px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker14.svg"
-      />
-      <img
-        className="absolute top-[75px] left-[286px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[44px] left-[296px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker27.svg"
-      />
-      <img
-        className="absolute top-[94px] left-[306px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[77px] left-[315px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[77px] left-[325px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[71px] left-[335px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[38px] left-[345px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker27.svg"
-      />
-      <img
-        className="absolute top-[53px] left-[355px] w-[7px] h-[6px]"
-        alt=""
-        src="../marker33.svg"
-      />
-      <img
-        className="absolute top-[90px] left-[364px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[91px] left-[374px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[61px] left-[385px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker36.svg"
-      />
-      <img
-        className="absolute top-[83px] left-[394px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[81px] left-[404px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[78px] left-[413px] w-[8px] h-[6px]"
-        alt=""
-        src="../marker39.svg"
-      />
-      <img
-        className="absolute top-[54px] left-[423px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker27.svg"
-      />
-      <img
-        className="absolute top-[94px] left-[434px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[69px] left-[443px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[46px] left-[453px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker43.svg"
-      />
-      <img
-        className="absolute top-[78px] left-[462px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[65px] left-[473px] w-[8px] h-[6px]"
-        alt=""
-        src="../marker45.svg"
-      />
-      <img
-        className="absolute top-[81px] left-[483px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[85px] left-[492px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
-      <img
-        className="absolute top-[89px] left-[502px] w-[7px] h-[7px]"
-        alt=""
-        src="../marker26.svg"
-      />
-      <img
-        className="absolute top-[89px] left-[512px] w-[8px] h-[7px]"
-        alt=""
-        src="../marker29.svg"
-      />
     </div>
   );
 };
