02dc8d1b6f2827900c42012f635869a0c3fb1c3e
paperhands-team <118849042+paperhands-team@users.noreply.github.com>
2022-11-27 10:52:47 -0500

    Revert "Feature/pap 18 integrate 3rd web"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
-import React from "react";
 import Logo from "../components/logo";
 import Auth from "../components/auth";
+import DarkModeButton from "../components/dark-mode-button";
 import App from "../components/app";
-import initializeFirebaseClient from "../lib/initFirebase";
+import React from "react";
 import useSWR from "swr";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
-import { signInWithCustomToken } from "firebase/auth";
-import { ConnectWallet, useAddress, useSDK } from "@thirdweb-dev/react";
 
 const fetcher = (url: RequestInfo | URL) =>
   fetch(url).then((res) => res.json());
@@ -16,76 +13,24 @@ const url =
   "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
 
 const Desktop1: NextPage = () => {
-  const address = useAddress();
-  const sdk = useSDK();
-
   // use SWR to fetch data from coingecko API
-
   const { data, error } = useSWR(url, fetcher);
-  if (data) {
-    console.log(data);
-  } else {
-    console.log(error);
-  }
-
-  const { db, auth } = initializeFirebaseClient();
-
-  async function signIn() {
-    const payload = await sdk?.auth.login("paperhands.app");
-
-    const res = await fetch("/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ payload }),
-    });
-
-    const { token } = await res.json();
-
-    signInWithCustomToken(auth, token).then((userCredential) => {
-      const user = userCredential.user;
-
-      const usersRef = doc(db, "users", user.uid!);
-
-      if (!usersRef) {
-        // user now exists in Firestore database
-        setDoc(
-          usersRef,
-          {
-            address: address,
-            createdAt: serverTimestamp(),
-          },
-          { merge: true }
-        );
-      }
-      //consolelog error
-    });
-  }
 
   return (
-    <div className=" [background:linear-gradient(180deg,_#2b303b,_rgba(43,_48,_59,_0))] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full overflow-hidden  lg:px-6 py-2.5">
-      <nav>
-        <div className="flex flex-wrap justify-around items-center mx-auto max-w-screen-xl">
-          <div className="flex items-center">
-            <Logo />
-          </div>
-          <div className="flex items-center">
-            {/* <Auth /> */}
-            <ConnectWallet />
-          </div>
+    <nav className="relative [background:linear-gradient(180deg,_#2b303b,_rgba(43,_48,_59,_0))] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full overflow-hidden  lg:px-6 py-2.5">
+      <div className="flex flex-wrap justify-around items-center mx-auto max-w-screen-xl">
+        <div className="flex items-center">
+          <Logo />
         </div>
-      </nav>
-
-      <div>
-        {address ? (
-          <button onClick={() => signIn()}>Sign in</button>
-        ) : (
-          <ConnectWallet />
-        )}
+        <div className="flex items-center">
+          <Auth />
+        </div>
       </div>
+
       <div className="flex items-center justify-center h-screen">
         <App />
       </div>
-    </div>
+    </nav>
   );
 };
 
