51790f1db6e880a18c47ef1d41e72cb493397d9a
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-11-27 00:48:18 -0500

    added thirdwebprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
 import "./global.css";
 
+const activeChainId = ChainId.Mainnet;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />;
+    </ThirdwebProvider>
+  );
 }
 
 export default MyApp;
